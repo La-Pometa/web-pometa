399513b4bd83c45a2a4999c8254b97093d90bc46
Jordi Gómez Hidalgo <gnugomez@gmail.com>
2022-04-22 14:44:50 +0200

    chore: add prose to default page single

--- components/single-type/page/default.vue
+++ components/single-type/page/default.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto margins space-y-8">
     <h1>{{ $content.getPostTitle(post) }}</h1>
     <the-content
-      class="space-y-6"
+      class="space-y-6 prose dark:prose-invert !max-w-none"
       :render="$content.getPostContent(post)"
     ></the-content>
   </div>
